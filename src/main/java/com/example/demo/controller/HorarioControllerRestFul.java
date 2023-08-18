e2f8efab533963d5bbbfcd221ba0c7c1aa683d69
Your Name <you@example.com>
2023-08-17 19:50:12 -0500

    Taller 41

--- src/main/java/com/example/demo/controller/HorarioControllerRestFul.java
+++ src/main/java/com/example/demo/controller/HorarioControllerRestFul.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.repository.modelo.Estudiante;
 import com.example.demo.repository.modelo.Horario;
 import com.example.demo.service.IHorarioService;
 
--- src/main/java/com/example/demo/service/EstudianteServiceImpl.java
+++ src/main/java/com/example/demo/service/EstudianteServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,12 @@ public class EstudianteServiceImpl implements IEstudianteService{
 
 	@Override
 	public void guardarEstudiante(Estudiante estudiante) {
+		try {
+			TimeUnit.SECONDS.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		this.estudianteRepository.insertarEstudiante(estudiante);		
 	}
 
